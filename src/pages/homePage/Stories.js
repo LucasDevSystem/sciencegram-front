8bb3e6395fc9d311576e3d107046e0073cfe3aeb
LucasDevSystem <highspeedsetup@gmail.com>
2022-08-04 13:49:58 -0300

    (Stories): onClick in Stories funcion

--- src/pages/homePage/Stories.js
+++ src/pages/homePage/Stories.js
@@ -5,33 +5,40 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
+function Stories({ storiesData = [] }) {
+  const onClick= (selectedUser) => {
+    console.log(selectedUser);
+  };
 
-function Stories({storiesData = []}) {
   return (
-    <Card sx={{ maxWidth: 500, display: "flex",width:500 }}>
-      {storiesData.map(({userName='',imageUrl=''},index) => {
-       return( <div key={index}>
-          <CardActionArea>
-            <CardMedia
-              style={{
-                marginLeft: 2,
-                marginTop: 5,
-                borderRadius: "50%",
-                borderColor: "green",
-                width: "80px",
-                borderStyle: "solid",
-                height: "80px",
-              }}
-              component="img"
-              image={imageUrl}
-            />
-            <CardContent style={{textAlign:'center'}}>
-              <Typography  gutterBottom variant="h7" component="div">
-               {userName.length>9?userName.slice(0, 7)+'...':userName}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </div>)
+    <Card sx={{ maxWidth: 500, display: "flex", width: 500 }}>
+      {storiesData.map(({ userName = "", imageUrl = "" }, index) => {
+        return (
+          <div key={index}>
+            <CardActionArea onClick={(e) => onClick(userName)}>
+              <CardMedia
+                style={{
+                  marginLeft: 2,
+                  marginTop: 5,
+                  borderRadius: "50%",
+                  borderColor: "green",
+                  width: "80px",
+                  borderStyle: "solid",
+                  height: "80px",
+                }}
+                component="img"
+                image={imageUrl}
+              />
+              <CardContent style={{ textAlign: "center" }}>
+                <Typography gutterBottom variant="h7" component="div">
+                  {userName.length > 9
+                    ? userName.slice(0, 7) + "..."
+                    : userName}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </div>
+        );
       })}
     </Card>
   );
