385ab462ae725768399c0fe2b06e4c231c54794b
LucasDevSystem <highspeedsetup@gmail.com>
2022-08-05 13:51:21 -0300

    (StoriesPage): implemented cardHeader and avatar

--- src/pages/stories/index.js
+++ src/pages/stories/index.js
@@ -5,9 +5,16 @@ import MobileStepper from "@mui/material/MobileStepper";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import { Avatar, CardHeader, IconButton } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StopIcon from '@mui/icons-material/Stop';
 document.body.style = "background:  #35425C;";
-
+const stories={
+    userName:'aa',
+    imagesUrl:[],
+}
 export default function StoriesPage() {
+  const [isStoped,setIsStoped] = React.useState(false);
   return (
     <div
       style={{
@@ -18,8 +25,28 @@ export default function StoriesPage() {
       }}
     >
       <Card sx={{ maxWidth: 800, width: 500 }}>
-        <MobileStepper
+        <CardHeader
           style={{ backgroundColor: "#465678" }}
+          avatar={
+            <Avatar
+              src={
+                "https://assets.reedpopcdn.com/scene-Iron-Man.webp/BROK/thumbnail/1600x900/format/jpg/quality/80/scene-Iron-Man.webp"
+              }
+              //  sx={{ bgcolor:  "#465678" }}
+              aria-label="recipe"
+            ></Avatar>
+          }
+          action={
+            <IconButton onClick={()=>setIsStoped(!isStoped)} aria-label="settings">
+             {isStoped? <PlayArrowIcon/>:<StopIcon></StopIcon>}
+            </IconButton>
+          }
+          title={"musk"}
+          //subheader={createdAt}
+        />
+
+        <MobileStepper
+          style={{ backgroundColor: "#465678",height:10 }}
           steps={5}
           position="static"
           activeStep={1}
